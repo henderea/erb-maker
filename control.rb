8ae430b4bcfefc4ffcd6c7cfc4d459427d66273d
Eric Henderson <henderea@gmail.com>
2017-08-28 13:36:56 -0400

    use the project directory instead of the working directory

--- control.rb
+++ control.rb
@@ -3,7 +3,7 @@
 require 'rubygems'
 require 'daemons'
 
-pwd = Dir.pwd
+pwd = __dir__
 Daemons.run_proc('erb-maker', {:dir_mode => :normal, :dir => '/opt/pids/sinatra'}) do
   Dir.chdir(pwd)
   exec "rackup -p 3746 --host 127.0.0.1"
